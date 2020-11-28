7ba6677828c1ab70c9375474385cdecee42342df
Piotrek Brzeziński <thewildtree@outlook.com>
2020-11-28 17:21:05 +0100

    Implement basic reporting modal

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:alpha_mobile/ui/report_sheet_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 import 'map/map_view.dart';
 
@@ -16,9 +18,13 @@ class HomeScreen extends StatelessWidget {
         widthFactor: 0.8,
         heightFactor: 0.1,
         child: FloatingActionButton.extended(
-          onPressed: () => {},
+          onPressed: () => showMaterialModalBottomSheet(
+              expand: false,
+              context: context,
+              builder: (context) => _reportBottomSheet(context)),
           tooltip: 'Report',
-          label: Text('REPORT', 
+          label: Text(
+            'REPORT',
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
           ),
           icon: Icon(Icons.report, size: 40),
@@ -26,6 +32,52 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  Widget _reportBottomSheet(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 30,
+        bottom: 20
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              bottom: 20
+            ),
+            child: Column(
+              children: [
+                Icon(Icons.pets, size: 48),
+                Container(height: 10),
+                Text(
+                  'The boar being reported is:',
+                  style: TextStyle(fontSize: 20)
+                ),
+                Container(height: 10),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ReportSheetButton(
+                icon: Icons.check, 
+                title: 'Alive', 
+                onTap: () => {}
+              ),
+              ReportSheetButton(
+                icon: Icons.close, 
+                title: 'Dead', 
+                onTap: () => {}
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
